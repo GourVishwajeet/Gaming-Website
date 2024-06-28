0aca6792299e8144b6cb09a0d6a8a6e09b12ca9b
Vishwajeet Gour <137927385+GourVishwajeet@users.noreply.github.com>
2024-06-28 09:08:20 +0000

    created app

---
+++ src/components/CardComponen.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function CardComponent (){
+    return(
+        <>
+        <a href="">
+            <div >
+
+            </div>
+        </a>
+        </>
+    )
+}
+
+
